986a4fa012620d0551c02dd881c768633deacc96
周晨光 <chenguang.zhou@chinaredstar.com>
2017-12-18 13:46:39 +0800

    添加face++ 中api常量

---
+++ src/main/java/com/chinaredstar/eating/components/constants/FaceURLContants.java
@@ -0,0 +1,19 @@
+package com.chinaredstar.eating.components.constants;
+
+public class FaceURLContants {
+
+    /**
+     * faceSet create api
+     */
+    public static final String CREATE_FACESET_API = "https://api-cn.faceplusplus.com/facepp/v3/faceset/create";
+
+    /**
+     * faceSet addFace api
+     */
+    public static final String ADD_FACE_FACESET_API ="https://api-cn.faceplusplus.com/facepp/v3/faceset/addface";
+
+    /**
+     * search face api
+     */
+    public static final String SEARCH_FACE_API = "https://api-cn.faceplusplus.com/facepp/v3/search";
+}
