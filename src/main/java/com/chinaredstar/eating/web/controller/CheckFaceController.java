adab204bdcda4ae850cf0845fa62a3640f0d6529
zhouyi <magic_023@163.com>
2017-12-19 19:22:23 +0800

    兼容base64

--- src/main/java/com/chinaredstar/eating/web/controller/CheckFaceController.java
+++ src/main/java/com/chinaredstar/eating/web/controller/CheckFaceController.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -27,19 +28,21 @@ public class CheckFaceController {
     private CheckFaceService checkFaceService;
     private static final Logger logger = LoggerFactory.getLogger(CheckFaceController.class);
     @RequestMapping(value = "/checkFace",method = RequestMethod.POST)
-    public RestResultVo checkFace(MultipartFile img){
-        if(img == null){
+    public RestResultVo checkFace(String base64,MultipartFile img){
+        if(img == null && StringUtils.isEmpty(base64)){
             return new RestResultVo(-1,"传入数据为空。");
         }
-        byte[] bytes;
-        try {
-            long start = System.currentTimeMillis();
-            bytes = img.getBytes();
-            logger.info("获取字节所花时间："+(System.currentTimeMillis()-start));
-        } catch (IOException e) {
-            return new RestResultVo(-1,"数据转换失败。");
+        byte[] bytes = null;
+        if(img != null){
+            try {
+                long start = System.currentTimeMillis();
+                bytes = img.getBytes();
+                logger.info("获取字节所花时间："+(System.currentTimeMillis()-start));
+            } catch (IOException e) {
+                return new RestResultVo(-1,"数据转换失败。");
+            }
         }
-        RestResultVo restResultVo = checkFaceService.checkFace(bytes);
+        RestResultVo restResultVo = checkFaceService.checkFace(base64,bytes);
         return restResultVo;
     }
 }
